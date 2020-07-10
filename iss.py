f9c22b77ca1ef59a8a9f3ba18fedc37df84ced0f
Michael DeMory <mjdemory2891@gmail.com>
2020-07-10 14:35:48 -0400

    Astro info and ISS location have correctly working code.

--- iss.py
+++ iss.py
@@ -1,10 +1,49 @@
 #!/usr/bin/env python
 
-__author__ = '???'
+__author__ = 'Michael DeMory'
+
+import requests
+import turtle
+
+def astro_info():
+    response = requests.get('http://api.open-notify.org/astros.json')
+
+    response.raise_for_status()
+
+    return response.json() ["people"]
+
+def location():
+    response = requests.get('http://api.open-notify.org/iss-now.json')
+    location = response.json()["iss_position"]
+    Lat = location["latitude"]
+    Long = location["longitude"]
+
+    return Lat, Long
+
+def iss_map():
+
+    screen = turtle.Screen()
+
+    screen.setup()
 
 
 def main():
-    pass
+    names = ''
+    astro_dict = astro_info()
+    for people in astro_dict:
+        names += people['name'] + ', '
+
+    print('Their names are: ' + names[:-2] + '.')
+
+    print("The current number of astronauts is {}.".format(len(astro_dict)))
+
+    Lat, Long = location()
+
+    print("Latitude: ", Lat)
+    print("Longitude: ", Long)
+
+
+
 
 
 if __name__ == '__main__':
