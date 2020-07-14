70c0b86f2e8d7d0f1bbe48431a2839d9e4bb8a73
Michael DeMory <mjdemory2891@gmail.com>
2020-07-14 13:35:28 -0400

    iss turtle moves to proper location and yellow dot shows Indianapolis location. Still need to add time of ISS overhead of Indy.

--- iss.py
+++ iss.py
@@ -4,6 +4,7 @@ __author__ = 'Michael DeMory'
 
 import requests
 import turtle
+import time
 
 def astro_info():
     response = requests.get('http://api.open-notify.org/astros.json')
@@ -15,16 +16,64 @@ def astro_info():
 def location():
     response = requests.get('http://api.open-notify.org/iss-now.json')
     location = response.json()["iss_position"]
-    Lat = location["latitude"]
-    Long = location["longitude"]
+    Lat = float(location["latitude"])
+    Long = float(location["longitude"])
 
     return Lat, Long
 
-def iss_map():
+def iss_map(Lat, Long):
 
     screen = turtle.Screen()
 
-    screen.setup()
+    screen.setup(width=720, height=360)
+
+    screen.setworldcoordinates(-180, -90, 180, 90)
+
+    screen.register_shape("iss.gif")
+
+    screen.bgpic("map.gif")
+
+    iss = turtle.Turtle()
+    iss.shape("iss.gif")
+    # iss.color('yellow')
+    iss.setheading(90)
+    iss.penup()
+    iss.goto(Long, Lat)
+    
+    
+
+    # Location of Indianapolis, IN
+    lat_indy = float(39.768452)
+    long_indy = float(-86.156212)
+
+    location_indy = turtle.Turtle()
+    location_indy.penup()
+    location_indy.color('yellow')
+    location_indy.goto(long_indy, lat_indy)
+    location_indy.dot(5)
+    location_indy.hideturtle()
+    screen.exitonclick()
+    return screen
+
+def overhead():
+
+    location_indy = turtle.Turtle()
+
+    parameters = {"lat": 39.768452, "long": -86.156212}
+
+    response = requests.get("http://api.open-notify.org/iss-pass.json", params=parameters)
+
+    data = response.json
+
+    over_indy = data['response'][1]['risetime']
+
+
+
+    # time.ctime(data['request']['risetime'])
+
+    style = ('Arial',6,'bold')
+
+    return location_indy.write(time.ctime(over_indy),font=style)
 
 
 def main():
@@ -42,7 +91,12 @@ def main():
     print("Latitude: ", Lat)
     print("Longitude: ", Long)
 
+    iss_map(Lat, Long)
 
+    overhead()
+
+    
+    
 
 
 
